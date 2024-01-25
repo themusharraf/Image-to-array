f8c5708d1c3c38ec86aeaf2eacad6c81c6145da5
msi <meibrohimov.@gmail.com>
2024-01-25 23:50:24 +0500

    new change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 image_path = 'taxi.jpg'  # O'zingizning tasvirning joylashgan manzilini yozing # noqa
 img = Image.open(image_path)
 
-# Tasvirni massivga o'zlashtirish
+# Tasvirni massivga o'zlashtirish  # noqa
 img_array = np.array(img)
 
 # Natijani tekshirish
